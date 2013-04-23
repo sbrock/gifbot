a8bfe03db48ade8e3bb70adb4452428ffab49acc
Adam Haapala <ahaapala@sithicus.ravenloft>
2013-04-23 12:13:22 -0400

    added topfive functionality

--- lib/gifbot.rb
+++ lib/gifbot.rb
@@ -15,8 +15,6 @@ module GifBot
         c.server   = options[:server]
         c.nick     = options[:nick]
         c.channels = options[:channels]
-        c.port = options[:port] if options[:port]
-        c.ssl.use = options[:ssl] if options[:ssl]
       end
       
       helpers do
@@ -25,7 +23,7 @@ module GifBot
           gifs    = []
           results = Nokogiri::HTML( open("#{GIFBIN_URL}/search/#{query}/") )
           
-          results.xpath('//div[@class="thumb-cell"]//a').each do |a|
+          results.xpath('//div[@class="thumbs"]//a').each do |a|
             gifs << a['href'].sub(/^\//, '')
           end
           
@@ -35,18 +33,52 @@ module GifBot
             id = gifs[rand(gifs.length)]
             page = Nokogiri::HTML( open("#{GIFBIN_URL}/#{id}") )
 
-            image_url_from_page(page)
+            image_url_from_page(page,1)
           end
         end
         
         def random
           page = Nokogiri::HTML(open("#{GIFBIN_URL}/random"))
           
-          image_url_from_page(page)
+          image_url_from_page(page,1)
         end
         
-        def image_url_from_page(page)
-          URI.escape(page.xpath('//div[@class="box"]//img[@class="gif"]').first['src'])
+        def image_url_from_page(page,count)
+          result_count = page.xpath('//*[@id="gif"]').count
+          print "result count #{result_count}\n"
+          puts page.xpath('//*[@id="gif"]').first['src']
+          urls = URI.escape(page.xpath('//*[@id="gif"]').shift['src'])
+          for i in 1..(count-1)
+            print "i: #{i}\n"
+            urls = "#{urls}\n" +URI.escape(page.xpath('//*[@id="gif"]').shift['src'])
+          end
+          urls
+        end
+
+        def topfive(query)
+
+          query   = CGI.escape(query)
+          gifs    = []
+          urls    = ""
+          results = Nokogiri::HTML( open("#{GIFBIN_URL}/search/#{query}/") )
+
+          results.xpath('//div[@class="thumbs"]//a').each do |a|
+            gifs << a['href'].sub(/^\//, '')
+          end
+
+          if gifs.empty?
+            "damn, no gif for \"#{query}\""
+          else
+            ids = gifs.sample(5)
+            for i in 1..5
+              id = ids[i]
+              page = Nokogiri::HTML( open("#{GIFBIN_URL}/#{id}") )
+              url = image_url_from_page(page,1)
+              urls << "\n"+image_url_from_page(page,1)
+
+            end
+          end
+	  urls
         end
       end
       
@@ -56,6 +88,10 @@ module GifBot
       
       on :message, /^?gifme (.+)/ do  |m, query|
         m.reply search(query)
+      end
+
+      on :message, /^?topfive (.+)/ do |m, query|
+        m.reply topfive(query)
       end
       
     end
